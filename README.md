ae92cd16b0abfc73b18202b9b66180615a3e25ff
Jeremy Mawson <jem.mawson@gmail.com>
2011-03-10 23:32:18 +1100

    newgame-pieces-positioned-refactor

--- README.md
+++ README.md
@@ -6,14 +6,11 @@ NewGameSpecification assertion that a new game has all pieces in the correct pos
 Test is "red" because there are no supporting classes and it can't compile. I've taken assumptions about how
 the API is to look, knowing full well that it may be refactored along the way.
 
-
-pieces-are-where-its-at-red:
-
-We start to fix NewGameSpecification by adding the NewGame type and a pieces value, but still we need to create the
-piece at behaviour. So we start a new specification to handle that.
-
-
 newgame-pieces-positioned-green:
+
+We start to fix NewGameSpecification by adding the NewGame type and the PlacedPiece type.
+
+newgame-pieces-positioned-refactor:
 
 NewGameSpecification passes because we have effectively just copied the assertion into the newly created NewGame type.
 Time to refactor?
--- src/main/scala/net/badgerhunt/typedchess/NewGame.scala
+++ src/main/scala/net/badgerhunt/typedchess/NewGame.scala
@@ -1,40 +1,15 @@
 package net.badgerhunt.typedchess
 
-import Position._
-
 object NewGame {
-  val pieces = List(
-    PlacedPiece(White, Rook, A1),
-    PlacedPiece(White, Knight, B1),
-    PlacedPiece(White, Bishop, C1),
-    PlacedPiece(White, Queen, D1),
-    PlacedPiece(White, King, E1),
-    PlacedPiece(White, Bishop, F1),
-    PlacedPiece(White, Knight, G1),
-    PlacedPiece(White, Rook, H1),
-    PlacedPiece(White, Pawn, A2),
-    PlacedPiece(White, Pawn, B2),
-    PlacedPiece(White, Pawn, C2),
-    PlacedPiece(White, Pawn, D2),
-    PlacedPiece(White, Pawn, E2),
-    PlacedPiece(White, Pawn, F2),
-    PlacedPiece(White, Pawn, G2),
-    PlacedPiece(White, Pawn, H2),
-    PlacedPiece(Black, Pawn, A7),
-    PlacedPiece(Black, Pawn, B7),
-    PlacedPiece(Black, Pawn, C7),
-    PlacedPiece(Black, Pawn, D7),
-    PlacedPiece(Black, Pawn, E7),
-    PlacedPiece(Black, Pawn, F7),
-    PlacedPiece(Black, Pawn, G7),
-    PlacedPiece(Black, Pawn, H7),
-    PlacedPiece(Black, Rook, A8),
-    PlacedPiece(Black, Knight, B8),
-    PlacedPiece(Black, Bishop, C8),
-    PlacedPiece(Black, Queen, D8),
-    PlacedPiece(Black, King, E8),
-    PlacedPiece(Black, Bishop, F8),
-    PlacedPiece(Black, Knight, G8),
-    PlacedPiece(Black, Rook, H8)
-  )
+
+  val pieces = {
+    val backRow = List(Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook)
+    val whiteBackRow = backRow.zip(Position.values.toList.filter(_.id % 8 == 0)).map(rp => PlacedPiece(White, rp._1, rp._2))
+    val whitePawnRow = Position.values.toList.filter(_.id % 8 == 1).map(p => PlacedPiece(White, Pawn, p))
+    val blackPawnRow = Position.values.toList.filter(_.id % 8 == 6).map(p => PlacedPiece(Black, Pawn, p))
+    val blackBackRow = backRow.zip(Position.values.toList.filter(_.id % 8 == 7)).map(rp => PlacedPiece(Black, rp._1, rp._2))
+
+    whiteBackRow ::: whitePawnRow ::: blackPawnRow ::: blackBackRow
+  }
+
 }
